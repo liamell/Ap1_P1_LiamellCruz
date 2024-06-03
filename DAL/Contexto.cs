a505b6c509ad98f54fb2b5349ea292dffcf07b18
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-06-03 17:33:39 -0400

    Conection String

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ap1_P1_LiamellCruz.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto>options) 
+            :base(options){ }
+
+       
+    }
+}
