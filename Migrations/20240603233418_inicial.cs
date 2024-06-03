91f69db4dd691d7dde6dbaa34fcb96e57638fbb3
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-06-03 19:35:49 -0400

    Migracion y actualizaion de la base de datos

---
+++ Migrations/20240603233418_inicial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ap1_P1_LiamellCruz.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
