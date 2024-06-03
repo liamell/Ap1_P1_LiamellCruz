ff3a690e1e0c59f86095d5fa1a831be0b8ed2959
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-06-03 19:05:55 -0400

    Hice el models

---
+++ Models/Articulos.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+
+namespace Ap1_P1_LiamellCruz.Models
+{
+    public class Articulos
+    {
+        [Key]
+
+        public int ArticulosId{ get; set; }
+		[Required(ErrorMessage = "Campo Obligatorio")]
+
+		public string Descripcion { get; set;}
+		[Required(ErrorMessage = "Campo Obligatorio")]
+
+		public decimal Costo { get; set;}
+		[Required(ErrorMessage = "Campo Obligatorio")]
+
+		public decimal Ganancia { get; set;}
+		[Required(ErrorMessage = "Campo Obligatorio")]
+
+		public decimal Precio { get; set; }
+		
+
+	}
+}
